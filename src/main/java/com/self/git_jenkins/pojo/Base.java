e52551e7658426a7448ede2fa34d3550eb88f3f1
Prankur Agarwal <prankur.agarwal18@gmail.com>
2020-04-25 20:02:31 +0530

    ADD: poc code for git integration

---
+++ src/main/java/com/self/git_jenkins/pojo/Base.java
@@ -0,0 +1,14 @@
+package com.self.git_jenkins.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder(toBuilder = true)
+@NoArgsConstructor
+@AllArgsConstructor
+public class Base {
+    private String ref;
+}
